f0dca57743a89f85b6e6356ddfd84f3646a90e16
madpugsijey <christian_josh_bueno@dlsu.edu.ph>
2025-11-11 15:31:10 +0800

    added Sales Summary and Product Sales Performance Reports

--- sql/04_report_queries.sql
+++ sql/04_report_queries.sql
@@ -4,14 +4,35 @@
 -- =================================================================
 -- Report: Sales Summary Report
 -- =================================================================
--- Add query here.
+SELECT 
+    DATE(transaction_date) AS sale_date,
+    COUNT(*) AS total_transactions,
+    SUM(net_amount) AS total_sales,
+    AVG(net_amount) AS average_sales
+FROM pos_transactions
+WHERE 
+    YEAR(transaction_date) = ? AND
+    MONTH(transaction_date) = ?
+    AND status = 'Completed'
+GROUP BY sale_date
+ORDER BY sale_date;
 
 
 -- =================================================================
 -- Report: Product Sales Performance Report
 -- =================================================================
--- Add query here.
-
+SELECT 
+    item_name,
+    SUM(quantity) AS total_quantity_sold,
+    SUM(line_total) AS total_revenue
+FROM pos_transaction_lines AS ptl
+JOIN pos_transactions AS pt ON ptl.transaction_id = pt.transaction_id
+WHERE 
+    YEAR(pt.transaction_date) = ? AND
+    MONTH(pt.transaction_date) = ?
+    AND pt.status = 'Completed'
+GROUP BY item_name
+ORDER BY total_revenue DESC;
 
 -- =================================================================
 -- Report: Discount Utilization Report
